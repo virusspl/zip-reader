fa1cf5ff3058a39d59a5f753add4379e3cf273f1
viruss <viruss@mail.pl>
2023-07-31 23:43:08 +0200

    init

---
+++ src/main/java/pl/devsoft/zipreader/ZipReaderApplication.java
@@ -0,0 +1,13 @@
+package pl.devsoft.zipreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/devsoft/zipreader/ZipReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.devsoft.zipreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
